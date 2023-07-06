0ba48ddf48279b2e58146916a7a0bac4edd8e8bd
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-07-05 23:25:09 -0500

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ export default defineConfig({
   base: SITE.basePathname,
   trailingSlash: SITE.trailingSlash ? 'always' : 'never',
 
-  output: 'static',
+  output: 'hybrid',
   adapter: vercel({
     analytics: true,
   }),
