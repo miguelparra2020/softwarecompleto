57250a1dc0ce2f2a9ebaa1fc0b348472a86785ed
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-08-15 20:41:11 -0500

    Update

--- src/components/chatmes.jsx
+++ src/components/chatmes.jsx
@@ -1,10 +1,41 @@
+import React, { useEffect } from 'react';
 
-const HolaComponente = () => {
+const FacebookMessengerChat = () => {
+  useEffect(() => {
+    // Configura el SDK de Facebook
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId: '100133298391116',
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: 'v17.0'
+      });
+    };
+
+    // Carga el SDK de Facebook
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }, []);
+
   return (
     <div>
-      <h1>Hola Miguel Paez componente jsx</h1>
+      {/* Contenido de tu aplicación */}
+      <h1>Mi Aplicación</h1>
+      
+      {/* Chat de Facebook Messenger */}
+      <div id="fb-root"></div>
+      <div
+        className="fb-customerchat"
+        attribution="setup_tool"
+        page_id="100133298391116"
+      ></div>
     </div>
   );
 };
 
-export default HolaComponente;
+export default FacebookMessengerChat;
