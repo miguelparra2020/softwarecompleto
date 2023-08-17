c004433de91ba281e623678985053801afa4b207
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-08-17 00:34:11 -0500

    Update

--- src/components/usuarios_time.jsx
+++ src/components/usuarios_time.jsx
@@ -4,9 +4,9 @@ const Usuario = () => {
   const [usuario, setUsuario] = useState('');
   const [fechaIngreso, setFechaIngreso] = useState('');
   const [horaIngreso, setHoraIngreso] = useState('');
-  const [tiempoPermanencia, setTiempoPermanencia] = useState('');
+  const [tiempoPermanencia, setTiempoPermanencia] = useState('00:00:00'); // Inicialización
+  const [tiempoInicio] = useState(new Date());
   const ruta = window.location.pathname;
-
 
   useEffect(() => {
     // Función para generar un nombre de usuario aleatorio
@@ -36,27 +36,7 @@ const Usuario = () => {
       return `${horaFormateada}`;
     };
 
-    // Función para calcular el tiempo de permanencia
-    const calcularTiempoPermanencia = () => {
-  const horaInicio = new Date();
-
-  // Actualizar el tiempo de permanencia cada segundo
-  const intervalo = setInterval(() => {
-    const horaActual = new Date();
-    const diferencia = horaActual - horaInicio;
-
-    const segundos = Math.floor((diferencia / 1000) % 60);
-    const minutos = Math.floor((diferencia / 1000 / 60) % 60);
-    const horas = Math.floor(diferencia / 1000 / 60 / 60);
-
-    const tiempoFormateado = `${horas}:${minutos}:${segundos}`;
-    setTiempoPermanencia(tiempoFormateado);
-  }, 1000);
-
-  return intervalo;
-};
     
-
     // Generar y establecer el nombre de usuario aleatorio
     const nombreUsuarioAleatorio = generarNombreUsuarioAleatorio();
     setUsuario(nombreUsuarioAleatorio);
@@ -69,65 +49,89 @@ const Usuario = () => {
     const horaIngresoActual = obtenerHoraIngreso();
     setHoraIngreso(horaIngresoActual);
 
-    // Calcular y establecer el tiempo de permanencia
-    const intervaloTiempo = calcularTiempoPermanencia();
     
-    return () => {
-    window.removeEventListener('beforeunload', handleBeforeUnload);
-    clearInterval(intervaloTiempo);
+    // Calcular el tiempo de permanencia en cada renderizado
+    const intervaloTiempo = setInterval(() => {
+      const horaActual = new Date();
+      const diferencia = horaActual - tiempoInicio;
+
+      const segundos = Math.floor((diferencia / 1000) % 60);
+      const minutos = Math.floor((diferencia / 1000 / 60) % 60);
+      const horas = Math.floor(diferencia / 1000 / 60 / 60);
+
+      const tiempoFormateado = `${String(horas).padStart(2, '0')}:${String(minutos).padStart(2, '0')}:${String(segundos).padStart(2, '0')}`;
+      setTiempoPermanencia(tiempoFormateado);
+    }, 1000);
+
+    // Manejar el evento beforeunload
+    const beforeUnloadHandler = () => {
+      // Calcular el tiempo de permanencia
+      const horaActual = new Date();
+      const diferencia = horaActual - tiempoInicio;
+
+      const segundos = Math.floor((diferencia / 1000) % 60);
+      const minutos = Math.floor((diferencia / 1000 / 60) % 60);
+      const horas = Math.floor(diferencia / 1000 / 60 / 60);
+
+      const tiempoFormateado = `${String(horas).padStart(2, '0')}:${String(minutos).padStart(2, '0')}:${String(segundos).padStart(2, '0')}`;
+
+      // Preparar los datos para enviar
+      const objetoDataUsuario2 = {
+        tiempo: tiempoFormateado,
+        ruta: ruta,
+        hora_ingreso: horaIngresoActual,
+        hora_salida: tiempoFormateado,
+        fecha_salida: fechaIngresoActual,
+        id: nombreUsuarioAleatorio,
+        fecha_ingreso: fechaIngresoActual
+      };
+      const dataObject = JSON.stringify(objetoDataUsuario2);
+
+      enviarDatos(dataObject);
+      datos(dataObject);
+      // Enviar los datos
+      
+      
     };
-  
-    
+
+
+    // Agregar el oyente del evento beforeunload
+    window.addEventListener('beforeunload', beforeUnloadHandler);
+
+
+    // Devolver una función de limpieza para eliminar el oyente cuando el componente se desmonte
+    return () => {
+      window.removeEventListener('beforeunload', beforeUnloadHandler);
+      clearInterval(intervaloTiempo);
+    };
   }, []);
 
-  const objetoDataUsuario2 = {
-    
-    tiempo: tiempoPermanencia,
-    ruta: ruta,
-    hora_ingreso: horaIngreso,
-    hora_salida: tiempoPermanencia,
-    fecha_salida: fechaIngreso,
-    id: usuario,
-    fecha_ingreso: fechaIngreso
-};
+  const datos = (dataObject) => {
+    console.log(dataObject)
+  }
+  
 
-console.log(JSON.stringify(objetoDataUsuario2));
-    // Función para enviar los datos mediante fetch
-    const enviarDatos = async () => {
-        try {
-          const response = await fetch('https://backendusuariostime.onrender.com/estadisticas/', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(objetoDataUsuario2)
-          });
-  
-          if (response.ok) {
-            console.log('Datos enviados exitosamente');
-            // Aquí puedes realizar cualquier acción adicional después de enviar los datos
-          } else {
-            console.error('Error al enviar los datos');
-          }
-        } catch (error) {
-          console.error('Error de red:', error);
-        }
-      };
-  
-      // Manejar el evento beforeunload
-      const handleBeforeUnload = (event) => {
-        event.preventDefault();
-  
-        // Llamar a la función enviarDatos cuando el usuario decide quedarse en la página
-        enviarDatos();
-      };
-  
-      // Agregar el listener para el evento beforeunload
-      window.addEventListener('beforeunload', handleBeforeUnload);
-  
+  const enviarDatos = async (dataObject) => {
+    try {
+      const response = await fetch('https://backendusuariostime.onrender.com/estadisticas/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: dataObject
+      });
+
+      if (response.ok) {
+        console.log('Datos enviados exitosamente');
+      } else {
+        console.error('Error al enviar los datos');
+      }
+    } catch (error) {
+      console.error('Error de red:', error);
+    }
+  };
 
   return (
-    
     <div>
       Hola soy usuario: {usuario}
       <br />
@@ -139,7 +143,6 @@ console.log(JSON.stringify(objetoDataUsuario2));
       <br />
       Tiempo de permanencia: {tiempoPermanencia}
       <br/>
-      {/* {objetoDataUsuario2} */}
     </div>
   );
 };
